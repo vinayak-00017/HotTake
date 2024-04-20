740f02c2b35446a1ff211c39c1477e47ef0b1365
vinayak <fobo401@gmail.com>
2024-04-20 15:20:33 +0530

    post

---
+++ components/Signup.tsx
@@ -0,0 +1,57 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+const Signup = () => {
+  return (
+    <Card className="w-[350px] rounded-xl">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Email</Label>
+              <Input
+                id="name"
+                placeholder="abc@xyz.com"
+                className="rounded-xl"
+              />
+            </div>
+            {/* <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="framework">Framework</Label>
+            <Select>
+              <SelectTrigger id="framework">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="next">Next.js</SelectItem>
+                <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                <SelectItem value="astro">Astro</SelectItem>
+                <SelectItem value="nuxt">Nuxt.js</SelectItem>
+              </SelectContent>
+            </Select>
+          </div> */}
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline" className="rounded-xl">
+          Continue
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default Signup;
--- components/appbar/Appbar.tsx
+++ components/appbar/Appbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { ModeToggle } from "./ModeToggle";
 import Profile from "./Profile";
 import Notifications from "./Notifications";
+import { SigninDialog } from "../SigninDailog";
 
 const Appbar = () => {
   return (
@@ -13,6 +14,7 @@ const Appbar = () => {
       <div>create</div>
       <Notifications></Notifications>
       <Profile></Profile>
+      <SigninDialog></SigninDialog>
     </header>
   );
 };
