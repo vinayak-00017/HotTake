9a6cf42f1addfb8c18a20ad4c47cc5b8228b04fc
vinayak <fobo401@gmail.com>
2024-04-26 09:38:14 +0530

    signup form

--- components/appbar/SigninDailog.tsx
+++ components/appbar/SigninDailog.tsx
@@ -15,15 +15,18 @@ import { signIn } from "next-auth/react";
 import { useCallback, useEffect, useState } from "react";
 import { Separator } from "../ui/separator";
 import { checkUsername } from "@/lib/actions/user";
+import Spinner from "../Spinner";
 
 export function SigninDialog() {
   const [username, setUsername] = useState<string>("");
   const [usernameOrEmail, setUsernameOrEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
   const [isSignup, setIsSignup] = useState(false);
-  const [email, setEmail] = useState<string>();
+  const [email, setEmail] = useState<string>("");
   const [isUsernameTaken, setIsUsernameTaken] = useState<boolean>();
   const [step, setStep] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [warning, setWarning] = useState<string>();
 
   const handleSignin = () => {
     signIn("credentials", { usernameOrEmail, password });
@@ -34,6 +37,12 @@ export function SigninDialog() {
   };
 
   const handleNext = () => {
+    if (isSignup && step == 1) {
+      if (!isValidEmail(email)) {
+        return setWarning("Enter a valid email address");
+      }
+    }
+    setWarning("");
     setStep((step) => step + 1);
   };
 
@@ -41,9 +50,16 @@ export function SigninDialog() {
     setStep((step) => step - 1);
   };
 
+  const isValidEmail = (email: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
   const handleUsernameChange = async (event: any) => {
     const newUsername = event.target.value;
     setUsername(newUsername);
+    setIsLoading(true);
 
     if (newUsername.length >= 3) {
       const usernameTaken = await checkUsername(event.target.value);
@@ -53,6 +69,7 @@ export function SigninDialog() {
       } else {
         setIsUsernameTaken(true);
       }
+      setIsLoading(false);
     }
   };
 
@@ -78,7 +95,7 @@ export function SigninDialog() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">{isSignup ? "Sign Up" : "Sign In"}</Button>
+        <Button variant="outline">Sign In</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
@@ -162,9 +179,15 @@ export function SigninDialog() {
                         className="col-span-3"
                       />
                     </div>
-                    {isUsernameTaken ? (
+                    {username.length < 3 ? (
+                      <div className="text-yellow-500">
+                        Username should be 3 or more characters
+                      </div>
+                    ) : isLoading ? (
+                      <Spinner></Spinner>
+                    ) : isUsernameTaken ? (
                       <div className="text-red-500">
-                        that username is already taken
+                        That username is already taken
                       </div>
                     ) : (
                       <div className="text-green-500">
@@ -190,6 +213,7 @@ export function SigninDialog() {
               )}
             </>
           )}
+          <div className=" text-red-500">{warning}</div>
           {step === 1 && (
             <div>
               <span>{!isSignup ? "New to Hottake?" : "Already spicy?"}</span>
